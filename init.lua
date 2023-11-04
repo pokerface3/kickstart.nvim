515bf8ab05e7c155bffde4f079a79c2cc0e2953c
Dave C <dave.cabral@broadcom.com>
2023-11-04 07:50:54 -0400

    Updates to init.lua
    Enabled relative line numbers
    Increased clipboard synchronization speed
    Saved undo history to a specific directory
    Decreased update time
    Commented out completeopt to menuone,noselect
    Enabled terminal GUI colors

--- init.lua
+++ init.lua
@@ -239,6 +239,7 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'r'
@@ -246,13 +247,14 @@ vim.o.mouse = 'r'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus' #REVIEW
 
 -- Enable break indent
 vim.o.breakindent = true
 
 -- Save undo history
 vim.o.undofile = true
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
@@ -262,11 +264,11 @@ vim.o.smartcase = true
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
+vim.o.updatetime = 50
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+-- vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
